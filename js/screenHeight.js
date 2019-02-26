cb6c013f183da5f279d269dc43f8d22fe9c2c2b1
yinyinflorence <869115528@qq.com>
2019-02-26 01:42:58 -0500

    niceformat

--- js/screenHeight.js
+++ js/screenHeight.js
@@ -46,60 +46,6 @@ $(function() {
 
 
 
-// $(function() {
-// 	 $(window).scroll(function() {
-// 		var element = $("#secondScreen");
-// 		var offset = element.position();
-// 		offset = offset.top-$(window).scrollTop();
-// 		if (offset < 200 && offset>-200) {
-// 			console.log("near");
-// 			titleDecoExpand2();
-// 		}
-// 	 });
-// });
-
-// $(function() {
-// 	 $(window).scroll(function() {
-// 		var element = $("#thirdScreen");
-// 		var offset = element.position();
-// 		offset = offset.top-$(window).scrollTop();
-// 		if (offset < 200 && offset>-200) {
-// 			console.log("close");
-// 			titleDecoExpand3();
-// 		}
-// 	 });
-// });
 
 
 
-
-
-// $(function() {
-// 	var statusArray = new Array(0,0,0,0,0,0,0,0);
-// 	 $(window).scroll(function() {
-// 	 	$(".imgCover").each(function(i) {
-// 	 		var offset = $(this).position().top-$(window).scrollTop();
-// 	 		console.log(statusArray[i]);
-
-// 	 		if (offset <=50 && offset >= 0 && statusArray[i] == 0) {
-// 			console.log("worksclose");
-// 			opacIncreaseSmallScreen(i);
-// 			statusArray[i] = 1;
-// 			// setTimeout(opacDecreaseSmallScreen(i),1000);
-// 		}
-// 			else if (offset<-50) {
-// 			opacDecreaseSmallScreen(i);
-// 			}
-// 	 	});	
-// 	 });
-// });
-
-
-// $(function() {console.log("infunction");$("#cover1").hover(opacIncrease1,opacDecrease1);});//为什么opacIncrease（参数不能用）
-// $(function() {console.log("infunction");$("#cover2").hover(opacIncrease2,opacDecrease2);});
-// $(function() {console.log("infunction");$("#cover3").hover(opacIncrease3,opacDecrease3);});
-// $(function() {console.log("infunction");$("#cover4").hover(opacIncrease4,opacDecrease4);});
-// $(function() {console.log("infunction");$("#cover5").hover(opacIncrease5,opacDecrease5);});
-// $(function() {console.log("infunction");$("#cover6").hover(opacIncrease6,opacDecrease6);});
-// $(function() {console.log("infunction");$("#cover7").hover(opacIncrease7,opacDecrease7);});
-// $(function() {console.log("infunction");$("#cover8").hover(opacIncrease8,opacDecrease8);});
